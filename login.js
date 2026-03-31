4ff0e48a549ca3763439e72c5bac85d3baa01b56
Madhumitha P <149236855+Madhumitha2312@users.noreply.github.com>
2026-03-31 21:10:24 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 const form = document.getElementById("loginForm");
 const message = document.getElementById("message");
 
@@ -7,16 +9,17 @@ form.addEventListener("submit", (e) => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    // simple validation
     if (!email || !password) {
         message.innerText = "Please fill all fields";
         return;
     }
 
-    // ✅ FAKE LOGIN SUCCESS
+    // Fake login success
     message.innerText = "Login successful ✅";
 
     setTimeout(() => {
         window.location.href = "dashboard.html";
     }, 1000);
+});
+
 });
