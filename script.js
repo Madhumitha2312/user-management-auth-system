2c0eb8a89053735e7fa65f4a46ffa6b28b947801
Madhumitha P <149236855+Madhumitha2312@users.noreply.github.com>
2026-03-31 20:39:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ return;
 
 try{
 
-const response = await fetch("/api/auth/register",{
+const response = await //fetch("/api/auth/register",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
